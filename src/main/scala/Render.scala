6a32aee32172a8dfd146dba20c633d6dcbcdcab3
Kirill Yuldashev <kirill.yuldashev@mail.ru>
2018-05-15 22:30:35 +0500

    rendering

--- src/main/scala/Render.scala
+++ src/main/scala/Render.scala
@@ -8,7 +8,7 @@ object Render {
   def apply(result: CommandResult): String =
     result match {
       case PollCreated(i) => s"Poll with $i id was successfully created."
-      case ListResult(l) => l.mkString("\n") // TODO: normal view
+      case ListResult(l) => viewList(l)
       case PollDeleted() => "Poll was successfully deleted."
       case PollStarted() => "Poll was successfully started."
       case PollStopped() => "Poll was successfully stopped."
@@ -21,20 +21,35 @@ object Render {
       case AnswerResult() => "Answer was accepted."
     }
 
+  private def viewList(l: List[(Int, Poll)]): String = {
+    val r = l.map(p => p._1 + s": '${p._2.name}'")
+    if (r.isEmpty) "Empty!" else r.mkString("\n")
+  }
+
   private def viewPoll(p: Poll): String =
-  s"""
-    |Name: ${p.name}
-    |Anonymity: ${anonymityMap(p.anonymity)}
-    |Visibility: ${visibilityMap(p.visibility)}
-    |Start time: ${p.startTime.getOrElse("None")}
-    |Stop time: ${p.stopTime.getOrElse("None")}
-    |Questions: ${p.questions.map(viewQuestion).mkString("\n")}
+  s"""Poll:
+     |  Name: ${p.name}
+     |  Anonymity: ${anonymityMap(p.anonymity)}
+     |  Visibility: ${visibilityMap(p.visibility)}
+     |  Start time: ${p.startTime.getOrElse("Not set")}
+     |  Stop time: ${p.stopTime.getOrElse("Not set")}
+     |  Questions:
+     |    ${p.questions.map(q => q.name + s" (${qType(q)})").mkString("\n    ")}
   """.stripMargin
 
+  private def qType(q: Question): String =
+    q match {
+      case _: OpenQuestion => "Open"
+      case _: MultiQuestion => "Multi"
+      case _: ChoiceQuestion => "Choice"
+    }
+
   private def viewQuestion(q: Question): String = {
-    s"""
-       |Name: ${q.name}
-       |Options: ${q.options.mkString("\n")}
+    val options = if (q.options.isEmpty) "Options is empty!" else q.options.mkString("\n")
+    s"""Name: ${q.name}
+       |Type: ${qType(q)}
+       |Options:
+       | $options
      """.stripMargin
   }
 }
